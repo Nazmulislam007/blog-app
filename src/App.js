df317a48d8b141c5d4a7a9082829cfe830078a55
nazmul <nazmulislam.ni897@gmail.com>
2022-05-15 16:40:52 +0600

    write data from firebase and store an image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import AddArticles from "./components/AddArticles";
 import Articles from "./components/Articles";
 
 function App() {
   return (
     <div className="App">
-      <Articles />
+      <div className="card__container">
+        <Articles />
+      </div>
+      <div className="card__form">
+        <AddArticles />
+      </div>
     </div>
   );
 }
--- src/components/AddArticles.js
+++ src/components/AddArticles.js
@@ -1,7 +1,100 @@
-import React from "react";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { db, storage } from "../firebase/firebase";
+import React, { useState } from "react";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { toast } from "react-toastify";
 
 const AddArticles = () => {
-  return <div>AddArticles</div>;
+  const [progress, setProgress] = useState(0);
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    image: "",
+    createAt: Timestamp.now().toDate(),
+  });
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const { title, description, image } = formData;
+    if (!title || !description || !image) return alert("Fill the all data");
+
+    const storageRef = ref(
+      storage,
+      `images/${new Date().getTime()}-${image.name}`
+    );
+
+    uploadBytesResumable(storageRef, image).on(
+      "state_changed",
+      (snapshot) => {
+        const progressPersentage =
+          Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progressPersentage);
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        setFormData({
+          title: "",
+          description: "",
+          image: "",
+        });
+        getDownloadURL(
+          uploadBytesResumable(storageRef, image).snapshot.ref
+        ).then((url) => {
+          const articleRef = collection(db, "Articles");
+          addDoc(articleRef, {
+            title,
+            description,
+            imageUrl: url,
+            createAt: Timestamp.now().toDate(),
+          })
+            .then(() => {
+              toast("Article added successfull", { type: "success" });
+              setProgress(0);
+            })
+            .catch(() => {
+              toast("Failed to added Article", { type: "error" });
+            });
+        });
+      }
+    );
+  };
+
+  const handleSubmit = (e) => {
+    const { value, name } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleImageSubmit = (e) => {
+    setFormData({ ...formData, image: e.target.files[0] });
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <h2>Add articles form</h2>
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={formData.title}
+        onChange={handleSubmit}
+      />
+      <textarea
+        name="description"
+        value={formData.description}
+        onChange={handleSubmit}
+      />
+      <input
+        type="file"
+        name="image"
+        accept="image/*"
+        onChange={handleImageSubmit}
+      />
+      <progress value={progress} max="100" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default AddArticles;
